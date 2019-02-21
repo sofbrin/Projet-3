c210a1c2f5615540df41ffe81ff01bea6286feef
sofbrin <sofbrin@hotmail.com>
2019-02-21 20:40:37 +0100

    update

--- tools.py
+++ tools.py
@@ -1,8 +1,9 @@
 class Tool:
 
-    def __init__(self, x, y, symbol):
+    def __init__(self, x, y, symbol, name):
         """ init tools """
         self.x = x
         self.y = y
         self.symbol = symbol
+        self.name = name
 
